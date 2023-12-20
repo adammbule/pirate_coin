d971f7b642fe0848d6a49bf9e234275123a1a496
adammbule <adammbule98@gmail.com>
2023-12-20 20:22:06 +0000

    Position & Named Arguments

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 void main(){//defining the function parameters when defining a function
-runApp(MaterialApp()//class
+runApp(MaterialApp(home: Text() )//class
 );//calling the function arguments when calling a function
 }
 
+void multi(a, b){
+    print (a * b);
+}
+
+multi(5, 7);
