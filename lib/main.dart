245e795925f7948fafa3b67a34621ac331f07d35
adammbule <adammbule98@gmail.com>
2023-12-25 11:47:35 +0000

    Scaffold & text

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,38 @@
 import 'package:flutter/material.dart';
+
+
 void main(){//defining the function parameters when defining a function
-runApp(MaterialApp(home: Text() )//class
+runApp(
+    const MaterialApp(
+        home: Scaffold(
+            body: Center(
+                child: Text('Hello  !!'),
+),
+),
+),//class
 );//calling the function arguments when calling a function
 }
 
-
+//Remove comments and run
+//const is used to optimize runtime of the application. --best practise
 //position argument
-void main(){
-    int multi(a, b){
-    return a * b;
-}
-int result = multi(5, 7);
-print (result);
-}
+/*void main(){
+   // int multi(a, b){
+    //return a * b;
+}*/
+//int result = multi(5, 7);
+//print (result);
+//}
 
 
 
 //named argument
 
-void main(){
-    int multi2({a, b}){
-        return a * b;
-    }
-  int result = multi2(a:5, b:5);
-    print (result);
-}
+/*void main(){
+    //int multi2({a, b}){
+     //   return a * b;
+    //}
+  //int result = multi2(a:5, b:5);
+   // print (result);
+}*/
 
