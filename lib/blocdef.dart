13d1c9873f5789faa611ec758a2d5652a5aecb66
Ch1ef <adammbule98@gmail.com>
2025-05-05 22:33:54 +0300

    prod variable

--- lib/blocdef.dart
+++ lib/blocdef.dart
@@ -1,5 +1,5 @@
 var auth = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MDk4ZDA0NzU0NjI5MDNlODRmMGZmNjAxYjQwZjRhNCIsIm5iZiI6MTcwNTk0MjA4Ny45NDU5OTk5LCJzdWIiOiI2NWFlOWM0NzNlMmVjODAwZWJmMDA3YTYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.m-rvfyxU5wUwRy8Z_jypbh2zfqubxpN_OuS8GVaNE48';
 var baseurl = 'https://api.themoviedb.org';
-//var baseurlfinal = 'https://piratenode.onrender.com/api';
-var baseurlfinal = 'http://localhost:4000/api';
+var baseurlfinal = 'https://piratenode.onrender.com/api';
+//var baseurlfinal = 'http://localhost:4000/api';
 //var baseurlfinal='https://chief-01.taildc4175.ts.net:4000/api';
